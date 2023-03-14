126db35b1eadf4760d81764fa5ba9dadc6b40abb
Artem Krupenko <Art1k.10@yandex.ru>
2023-03-14 16:51:36 +0300

    Initial Commit

---
+++ AppSwiftUI/AppSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppSwiftUIApp.swift
+//  AppSwiftUI
+//
+//  Created by Артем on 14.03.2023.
+//
+
+import SwiftUI
+
+@main
+struct AppSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
